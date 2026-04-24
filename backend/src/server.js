d6b0a94b32339557ec3fdff97c3ff73afed71179
Sahil Singh <sahil.rajesh.singh007@gmail.com>
2026-04-24 14:47:38 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,11 @@ dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
 
 // Middleware
 app.use(cors({
